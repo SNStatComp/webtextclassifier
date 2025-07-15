65eda53ac444f86ae72e276844e7f9816eae6f4a
lhaarman <luukhaarman@gmail.com>
2025-07-15 14:52:11 +0000

    setup experiment

--- src/LLM/llm_embedder.py
+++ src/LLM/llm_embedder.py
@@ -6,5 +6,6 @@ def create_model(config):
     return model
 
 
-def embed_text(model, text):
-    return model.encode(text)
+def embed_text(model, text, variable):
+    prompt = f"{text}, does the above-mentioned text, relating to a web-page, contain text with its main content about: {variable}?"
+    return model.encode(prompt)
---
+++ src/sandbox.py
@@ -0,0 +1,118 @@
+import s3fs
+import json
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from omegaconf import OmegaConf
+from util import setup, extract_text
+from LLM import classify, create_model, embed_text
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.linear_model import LogisticRegression
+from datetime import datetime
+
+
+def main():
+    config = setup("config/config.yaml")
+
+    print("Config:")
+    print(OmegaConf.to_yaml(config))
+
+    # TODO this assumes the use of Onyxia, change later to something more generic to read input file and variables
+    # Create filesystem object
+    fs = s3fs.S3FileSystem(
+        client_kwargs={'endpoint_url': config.filesystem.endpoint},
+        key=config.aws.access_key,
+        secret=config.aws.secret_access_key,
+        token=config.aws.session_token
+    )
+    BUCKET = config.filesystem.bucket
+    FILE_KEY_S3 = config.input.input_file
+    FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
+
+    # Read the URL dataframe containing URL and country code 
+    with fs.open(FILE_PATH_S3, mode="rb") as file_in:
+        url_data = pd.read_csv(file_in, sep=",")
+
+    # Read the variables for which we want to see if this text contains the content
+    with open(config.input.input_dir + "/" + config.input.input_variables, "r", encoding="utf-8") as file:
+        variables = file.readlines()
+
+    url_country = list(zip(url_data['url'], url_data['country']))
+    random.shuffle(url_country)
+
+    # Only used for testing on a small sample to speed things up
+    N = 25  # Sample size of list
+    url_country = url_country[:N]
+
+    models = None
+    if config.llm.method == "embedding":
+        model_embed = create_model(config)
+
+        # TODO replace with code to read model config/load in pre-trained model
+        model_class = LogisticRegression(max_iter=0)
+        model_class.fit(np.random.rand(2, config.llm.embedding.dim), [0, 1])
+        models = {
+            "embed": model_embed,
+            "class": model_class
+        }
+
+    embeddings = {}
+    for var in variables:
+        embeddings[var] = []
+        output = {}
+        print("Variable:", var)
+        for i, uc_tuple in enumerate(url_country):
+            url, country = uc_tuple
+            print(f"url {i + 1}/{len(url_country)}: {url}")
+            try:
+                extracted_text = extract_text(config, url, country)
+            except Exception as e:
+                print(e)
+                continue
+
+            if len(extracted_text) < 1:
+                # print(f"Issue extracting text for url: {url}, language: {language}")
+                continue
+            
+            embedding = embed_text(models["embed"], extracted_text, var)
+            embeddings[var].append(embedding)
+            response = classify(config, extracted_text, var, models)
+            output[url] = response
+
+        # print("# times LLM output exceeded specified maximum length")
+        # print(len([v for v in output.values() if len(v) > 3]))
+        print(f"Total items: {len(output)}/{len(url_country)}")
+
+        print(output)
+
+        with open(f"{config.output.output_dir}/output_{var.replace(" ", "_").lower()}_{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.json", "w") as fp:
+            json.dump(output, fp)
+
+    print("Done!")
+    print(embeddings)
+    np.save("embeddings_pickly.npy", embeddings)
+
+    first_embedding = embeddings[list(embeddings.keys())[0]][0]
+
+    for key in embeddings.keys():
+        embeddings_key = embeddings[key]
+        plot_similarity_distribution(first_embedding, embeddings_key, key)
+
+
+def plot_similarity_distribution(first_embedding, embeddings, title):
+    similarities = cosine_similarity([first_embedding], embeddings)
+    similarities = similarities.flatten()
+    
+    plt.figure(figsize=(8, 5))
+    plt.hist(similarities, bins=30, color='skyblue', edgecolor='black')
+    plt.title(title)
+    plt.xlabel('Cosine Similarity')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    plt.savefig(f"{title}.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
