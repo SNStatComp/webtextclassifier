a0f36dc9c71851fb3088277f2807840a1e0b4632
lhaarman <luukhaarman@gmail.com>
2025-07-16 14:30:20 +0000

    continuation experiments

--- src/LLM/llm_api.py
+++ src/LLM/llm_api.py
@@ -3,14 +3,14 @@ import openai
 
 def prompt_LLM(config, extracted_text="", variable=None):
     client = openai.OpenAI(
-        api_key=config.llm.api.key,
-        base_url=config.llm.api.url
+        api_key=config.llm.prompt.api_key,
+        base_url=config.llm.prompt.url
     )
 
     prompt_content = f"{extracted_text}, does the above-mentioned text, relating to a web-page, contain text with its main content about: {variable}?"
 
     response = client.chat.completions.create(
-        model = config.llm.model,
+        model = config.llm.prompt.model,
         messages = [
             {"role":"system", "max_completion_tokens":"1", "content":"You are an expert at classifying texts, your answer is a one-word response, chosen from [Yes, No]."},
             {"role":"user", "content": prompt_content},
