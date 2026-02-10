b99bb13693a4ffe1aed61d9e67ea1fc74fe7e6bd
lhaarman <luukhaarman@gmail.com>
2026-02-10 16:11:33 +0000

    initial refactoring for PoC

--- src/LLM/__init__.py
+++ src/LLM/__init__.py
@@ -1,3 +1,26 @@
-from .llm_api import prompt_LLM
-from .llm_classifier import classify
-from .llm_embedder import create_model, embed_text
+from .llm_classifier import create_classification_model, classify_prompt, classify_embedding
+from .llm_embedder import create_embedding_model
+import warnings
+
+
+# Generic function to create models
+def create_model(config, model_type):
+    match model_type:
+        case "embedder":
+            return create_embedding_model(config)
+        case "classifier":
+            return create_classification_model(config)
+        case _:
+            warnings.warn(f"Model type {model_type} has no defined model creation method!")
+            return None
+
+
+# Generic function to classify text
+def classify(config, extracted_text, variable=None, models=None):
+    match config.llm.method:
+        case "prompt":
+            return classify_prompt(config, extracted_text, variable)
+        case "embedding":
+            return classify_embedding(config, extracted_text, models)
+        case _:
+            raise Exception(f"Unspecified method for classification: {config.llm.method}")
--- src/LLM/llm_embedder.py
+++ src/LLM/llm_embedder.py
@@ -1,11 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
 
-def create_model(config):
+def create_embedding_model(config):
+    # TODO loading weights from dir
+    print("Loading SentenceTransformer with weights...")
     model = SentenceTransformer(config.llm.embedding.model)
     return model
 
 
-def embed_text(model, text, variable):
-    prompt = f"{text}, does the above-mentioned text, relating to a web-page, contain text with its main content about: {variable}?"
-    return model.encode(prompt)
+def embed_text(model, text):
+    return model.encode(text)
