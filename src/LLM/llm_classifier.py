0e5639e122821641efefb9824be4d3c8bb363127
lhaarman <luukhaarman@gmail.com>
2026-03-12 13:08:12 +0000

    SVM added + poc main edits

--- src/LLM/llm_classifier.py
+++ src/LLM/llm_classifier.py
@@ -2,6 +2,7 @@ from .llm_prompter import prompt_LLM
 from .llm_embedder import embed_text
 import warnings
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 import joblib
 
 
@@ -13,8 +14,12 @@ def create_classification_model(config, pretrained=True):
                 # We use joblib for loading pkls
                 model = joblib.load(config.llm.embedding.classifier_path)
                 print(f"Loaded weights for {config.llm.embedding.classifier} classifier")
-            else:
-                model = DecisionTreeClassifier()
+            return model
+        case "SVM_rbf":
+            model = SVC(kernel="rbf")
+            if pretrained:
+                model = joblib.load(config.llm.embedding.classifier_path)
+                print(f"Loaded weights for {config.llm.embedding.classifier} classifier")
             return model
         case _:
             warnings.warn(f"TODO! {config.llm.embedding.classifier} not yet supported")
@@ -29,4 +34,4 @@ def classify_prompt(config, extracted_text, variable):
 def classify_embedding(config, extracted_text, models):
     embedding = embed_text(models["embedding"], extracted_text)
     prediction = models["classification"].predict(embedding)
-    return prediction
+    return prediction.astype(float).astype(int).astype(str)
